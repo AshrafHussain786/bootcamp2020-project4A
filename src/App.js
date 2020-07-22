3839d8ea2c6f865e872a35910ad429f3119d7c66
Muhammad Ashraf <ashrafhussain786@yahoo.com>
2020-07-22 21:06:37 +0500

    project 4A commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,130 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import useWebAnimations from "@wellyshen/use-web-animations";
+import car from './images/car.png';
+import wheels from './images/wheel.png';
+import clouds from './images/cloud.gif';
+import bird1 from './images/bird.gif';
 
-function App() {
+function App() {  
+  const highwayAnim = useWebAnimations({
+    keyframes: [
+        {transform: 'translateX(-1600px)'},        
+    ],
+    timing: {
+      duration: 5000,
+      iterations: Infinity,
+      easing: "linear",
+    }
+  });
+  const cityAnim = useWebAnimations({
+    keyframes: [
+        {transform: 'translateX(-1400px)'},        
+    ],
+    timing: {
+      duration: 20000,
+      iterations: Infinity,
+      easing: "linear",
+    }
+  });
+  const carAnim = useWebAnimations({
+    keyframes: [
+        {transform: 'translateY(-1px)'},        
+        {transform: 'translateY(1px)'},        
+        {transform: 'translateY(-1px)'},        
+    ],
+    timing: {
+      duration: 1000,
+      iterations: Infinity,
+      easing: "linear",
+    }
+  });
+  const wheel1Anim = useWebAnimations({
+    keyframes: [
+        {transform: 'rotate(0deg'},
+        {transform: 'rotate(360deg'},                 
+    ],
+    timing: {
+      duration: 1000,
+      iterations: Infinity,
+      easing: "linear",
+    }
+  });
+  const wheel2Anim = useWebAnimations({
+    keyframes: [
+        {transform: 'rotate(0deg'},
+        {transform: 'rotate(360deg'},                 
+    ],
+    timing: {
+      duration: 1000,
+      iterations: Infinity,
+      easing: "linear",
+    }
+  });
+  const cloud1Anim = useWebAnimations({
+    keyframes: [
+      { transform: 'translate(1000px,0px)' },
+      { transform: 'translate(-400px,0px)' }
+    ],
+    timing: {
+      duration: 15000,
+      iterations: Infinity,
+      easing: "linear",
+    }
+  });
+  const cloud2Anim = useWebAnimations({
+    keyframes: [
+      { transform: 'translate(1000px,0px)' },
+      { transform: 'translate(-400px,0px)' }              
+    ],
+    timing: {
+      duration: 15000,
+      iterations: Infinity,
+      easing: "linear",
+    }
+  });
+  const cloud3Anim = useWebAnimations({
+    keyframes: [
+      { transform: 'translate(1000px,0px)' },
+      { transform: 'translate(-400px,0px)' }              
+    ],
+    timing: {
+      duration: 15000,
+      iterations: Infinity,
+      easing: "linear",
+    }
+  });
+  const birdAnim = useWebAnimations({
+    keyframes: [
+      { transform: 'translate(0,0px)' },
+    { transform: 'translate(1500px,0px)' }              
+    ],
+    timing: {
+      duration: 20000,
+      iterations: Infinity,      
+      easing: "linear",
+    }
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bird-div">
+        <img src={bird1} ref={birdAnim.ref} alt="bird" className="bird" />        
+      </div>
+      <div className="cloud">
+        <img src={clouds} ref={cloud1Anim.ref} alt="cloud" />
+        <img src={clouds} ref={cloud2Anim.ref} alt="cloud" />
+        <img src={clouds} ref={cloud3Anim.ref} alt="cloud" />
+      </div>      
+      <div className="highway" ref={highwayAnim.ref}></div>
+      <div className="city" ref={cityAnim.ref}></div>
+      <div className="car" ref={carAnim.ref}>        
+          <img src={car} alt="car" />
+      </div>
+      <div className="wheels">
+          <img src={wheels} ref={wheel1Anim.ref} alt="car" className="back-wheel" />
+          <img src={wheels} ref={wheel2Anim.ref} alt="car" className="front-wheel" />
+      </div>
     </div>
   );
 }
-
 export default App;
